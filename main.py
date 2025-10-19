37ce10f12825e9e7d64eb3b84058de8b161304f7
Drew Giffin <drewgiffin97@gmail.com>
2025-10-19 13:41:30 -0400

    Missing data is reported and cleaned

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ data_path = "student_lifestyle_dataset.csv"
 def main():
     df = load_data()
     inspect_data(df)
+    df_clean = clean_data(df)
     
 def load_data():
     df = pd.read_csv(data_path, encoding="ascii", delimiter=",")
@@ -22,5 +23,13 @@ def inspect_data(df):
     print("Description:")
     print(df.describe(include="all"))
     print("\n")
+
+def clean_data(df):
+    print("Missing values:")
+    print(df.isnull().sum())
+    print("\n")
+    
+    df_clean = df.dropna(inplace=False)
+    return df_clean
 
 main()
